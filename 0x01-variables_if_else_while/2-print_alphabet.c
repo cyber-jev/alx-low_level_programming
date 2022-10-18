aff819c665aa3aa562bae10b44d676c23996241c
Johnny Ekparam <johnnye4u2c@gmail.com>
2022-10-17 23:20:58 -0700

    all

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,5 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
+/**
+ * main - main block
+ * Description: print out the alphabets
+ */
 int main(void)
 {
 	char alp = 'a';
@@ -11,4 +15,4 @@ int main(void)
 	}
 	putchar('\n');
 	return (0);
-}
+i}
